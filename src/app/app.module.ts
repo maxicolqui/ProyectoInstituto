86bf6d44168cde31abe3a96bbbbea23cdebfc4db
Arturo Maximiliano  Colqui <maximilianocolqui@gmail.com>
2023-09-05 23:25:01 -0300

    #

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,7 @@ import { HomeModule } from './home/home.module';
 import { SoftwareModule } from './software/software.module';
 import { TurnoModule } from './turno/turno.module';
 import { ClassModule } from './class/class.module';
-<<<<<<< HEAD
 import { ReactiveFormsModule } from '@angular/forms';
-=======
->>>>>>> c4547bc6fba5beaa1a06282961a93f3def1b77f7
 
 @NgModule({
   declarations: [
@@ -23,13 +20,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     SoftwareModule,
     TurnoModule,
     ClassModule,
-<<<<<<< HEAD
     ReactiveFormsModule
   
-=======
-    
-
->>>>>>> c4547bc6fba5beaa1a06282961a93f3def1b77f7
   ],
   providers: [],
   bootstrap: [AppComponent]
