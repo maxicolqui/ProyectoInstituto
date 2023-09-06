dc47072d7ecef4fa880aab49b8cfd6704dc9f79a
Arturo Maximiliano  Colqui <maximilianocolqui@gmail.com>
2023-09-05 23:16:11 -0300

    #

--- src/app/software/software.module.ts
+++ src/app/software/software.module.ts
@@ -3,18 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SoftComponent } from './soft/soft.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { AppRoutingModule } from '../app-routing.module';
-<<<<<<< HEAD
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-=======
-import { FormsModule } from '@angular/forms';
->>>>>>> c4547bc6fba5beaa1a06282961a93f3def1b77f7
 
 
 
 @NgModule({
   declarations: [
     SoftComponent,
-<<<<<<< HEAD
     InicioComponent,
   ],
   imports: [
@@ -22,12 +17,6 @@ import { FormsModule } from '@angular/forms';
     FormsModule,
     AppRoutingModule,
     ReactiveFormsModule,
-=======
-    InicioComponent
-  ],
-  imports: [
-    CommonModule,
->>>>>>> c4547bc6fba5beaa1a06282961a93f3def1b77f7
     FormsModule
   ],
   exports:[
