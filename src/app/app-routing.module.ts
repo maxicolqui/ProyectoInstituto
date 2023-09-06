ad54854a7f06c725f69bd989e18d815d7b044d3a
Arturo Maximiliano  Colqui <maximilianocolqui@gmail.com>
2023-09-05 23:05:23 -0300

    #

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,8 @@ import { ErrorComponent } from './home/error/error.component';
 import { SoftComponent } from './software/soft/soft.component';
 import { InicioComponent } from './software/inicio/inicio.component';
 import { ReservaComponent } from './turno/reserva/reserva.component';
-<<<<<<< HEAD
 import { Module1Component } from './courses/module1/module1.component';
 import { NosotrosComponent } from './nosotros/nosotros/nosotros.component';
-=======
->>>>>>> c4547bc6fba5beaa1a06282961a93f3def1b77f7
 
 
 const routes: Routes = [
@@ -18,11 +15,8 @@ const routes: Routes = [
   {path:"software",component:SoftComponent},
   {path:"logeo",component:InicioComponent},
   {path:"pagina",component:ReservaComponent},
-<<<<<<< HEAD
   {path:"Contacto",component:Module1Component},
   {path:"recuperacion",component:NosotrosComponent},
-=======
->>>>>>> c4547bc6fba5beaa1a06282961a93f3def1b77f7
   {path:"**",component:ErrorComponent}
 ];
 
