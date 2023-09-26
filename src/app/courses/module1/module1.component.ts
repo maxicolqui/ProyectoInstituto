77b8441a0eadd37878dbb1e531d81bcde121abf4
Arturo Maximiliano  Colqui <maximilianocolqui@gmail.com>
2023-09-25 23:31:53 -0300

    actualizacion de ngfor en las cars

--- src/app/courses/module1/module1.component.ts
+++ src/app/courses/module1/module1.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { listaMember } from 'src/app/data/datos';
+import { Member } from 'src/app/models/member';
 
 @Component({
   selector: 'app-module1',
   templateUrl: './module1.component.html',
   styleUrls: ['./module1.component.css']
 })
-export class Module1Component {
+export class Module1Component implements OnInit {
+
+  listaMember1: Member[] = listaMember;
+    title = 'team';
+  t:any;
+  constructor( private router : Router){
+
+  }
+
+  ngOnInit(): void {
+    console.log(listaMember1)
+  }
 
 }
---
+++ src/app/models/member.ts
@@ -0,0 +1,11 @@
+export class Member{
+    id: number | undefined;
+    nombre: string | undefined;
+    imagen: string | undefined;
+    nombreYap:string | undefined;
+    tel:string | undefined;
+    mail: string | undefined;
+    git : string | undefined;
+    linkedin : string | undefined;
+    
+}
